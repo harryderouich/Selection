4d3c513db868a3b4ff1993628b3272d0db245239
Harry Derouich <Harry Derouich@HarryDerouichPC.home>
2014-10-02 21:07:39 +0100

    First commit - finished

---
+++ Are these numbers the same.py
@@ -0,0 +1,14 @@
+#Harry Derouich
+#02/10/14
+#Are these two numbers the same?
+
+print("Hello user")
+number1 = int(input("Please enter a number: "))
+number2 = int(input("Please enter another number: "))
+
+if number1 == number2:
+    print("The numbers are the same")
+else:
+    print("The numbers are not the same")
+
+    
