9df6c50ebcada9a00f1e989a866ae76eae115859
harryderouich <40683@student.longroad.ac.uk>
2014-10-03 14:32:12 +0100

    Second commit - month and day done;  year still needs work!

--- ddmmyy program.py
+++ ddmmyy program.py
@@ -2,9 +2,10 @@
 #03/10/14
 #Stretch and challenge exercise 2
 
-date = input("Please enter the date in the format dd mm yy: ")
+date = input("Please enter the date in the format dd mm yy, (e.g. 10th October 1997 = 10 10 97): ")
 
-day = int(date[:3])
+#string splitting
+day = int(date[:2])
 month = int(date[3:5])
 year = int(date[6:])
 
@@ -12,54 +13,82 @@ print(day)
 print(month)
 print(year)
 
-#if day == "01":
-  #  day_format = "1st"
-#elif day == "02":
- #   day_format = "2nd"
-#elif day == "03":
- #   day_format = "3rd"
-#else:
-   # day_format = "error"
-#^^Come back to this later
-    #print(day_format)
+##day calculations below##
+if day == 0o1:
+    day_format = ("1st")
+elif day == 0o2:
+    day_format = ("2nd")
+elif day == 0o3:
+    day_format = ("3rd")
+elif day == 21:
+    day_format = ("21st")
+elif day == 22:
+    day_format = ("22nd")
+elif day == 23:
+    day_format = ("23rd")
+elif day == 31:
+    day_format = ("31st")
+else:
+    day_format = ("{0}th".format(day))
+print(day_format)   
 
-#month calculations below
-if month == "01":
+##end month calculations##
+
+
+###month calculations below###
+
+if month >= 10:
+    month = month
+else:
+    month = date[4:5]
+
+if month == ("1"):
     month_format = "January"
-elif month == "02":
+elif month == ("2"):
     month_format = "February"
-elif month == "03":
+elif month == ("3"):
     month_format = "March"
-elif month == "04":
+elif month == ("4"):
     month_format = "April"
-elif month == "05":
+elif month == ("5"):
     month_format = "May"
-elif month == "06":
+elif month == ("6"):
     month_format = "June"
-elif month == "07":
+elif month == ("7"):
     month_format = "July"
-elif month == "08":
+elif month == ("8"):
     month_format = "August"
-elif month == "09":
+elif month == ("9"):
     month_format = "September"
-elif month == "10":
+elif month == 10:
     month_format = "October"
-elif month == "11":
+elif month == 11:
     month_format = "November"
-elif month == "12":
+elif month == 12:
     month_format = "December"
 else:
     month_format = "This isnt working"
 
-#year calculations below
+print(month_format)
 
-if 20 >= year >= 99:
-    year_format = ("19{0}".format(year))
-elif 0 >= year >= 19:
-    year_format = ("20{0}".format(year))
-else:
-    year_format = ("This isnt working")
+###end month calculations###
 
-print(year_format)
+
+
+####year calculations below####
+#if year == 00:
+   # year_format = "2000"
+#elif year == 01:
+   # year_format = "2001"
+#else:
+   # year_format = "I'll do this later"
+
+#print(year_format)
+
+####end year calculations####
+
+print("The date is: {0} {1}".format(day_format, month_format))
+
+
 
     
