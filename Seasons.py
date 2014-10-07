cad167b4fdccc94ececb7424c6eddada224a0c89
harryderouich <40683@student.longroad.ac.uk>
2014-10-07 13:00:28 +0100

    First commit - Needs work

---
+++ Seasons.py
@@ -0,0 +1,9 @@
+#Harry Derouich
+#07/10/14
+#Python School Additional Exercises 2: Ex 3/4
+
+print("Hello user")
+
+month = input("Please enter a month")
+
+if month == "January" or month == "December" or month == "Febuary"
